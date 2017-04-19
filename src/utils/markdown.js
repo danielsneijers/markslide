e0f9618e51005921c4994141fd4cccad95615055
Daniel Sneijers <daniel.sneijers@gmail.com>
2017-04-19 15:41:16 +0200

    add highlighting and setup for themes

--- src/utils/markdown.js
+++ src/utils/markdown.js
@@ -1,10 +1,16 @@
 import marked from 'marked'
 import { memoize, compose } from 'ramda'
 
+marked.setOptions({
+  highlight: function (code) {
+    return require('highlight.js').highlightAuto(code).value
+  }
+})
+
 export const splitSlides = memoize((content) => content.split('\n---\n'))
 export const slidesCount = memoize((content) => splitSlides(content).length)
 export const parseSlides = memoize((slides) =>
-slides.map((slide) =>
-marked(slide)))
+  slides.map((slide) =>
+    marked(slide)))
 
 export const parse = compose(parseSlides, splitSlides)
