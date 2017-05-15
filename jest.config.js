5ebe64d56f2d43853ed664c01b209733fb6adf42
Daniel Sneijers <daniel.sneijers@gmail.com>
2017-05-15 17:04:26 +0200

    update to new jest configuration

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  'moduleFileExtensions': [
+    'js'
+  ],
+  'moduleDirectories': [
+    'node_modules',
+    'src'
+  ],
+  'moduleNameMapper': {
+    '^config$': '<rootDir>/config',
+    '\\.css$': 'identity-obj-proxy',
+    '\\.md$': '<rootDir>/config/__mocks__/slides.js'
+  },
+  'snapshotSerializers': ['enzyme-to-json/serializer']
+}
